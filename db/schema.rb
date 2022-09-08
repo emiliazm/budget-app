a66ff5112384676c705ffbeff3941ed613f07f35
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-09-08 14:04:52 +0200

    Fix groups_operations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_183244) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_153424) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "group_operacions", force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "operation_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_group_operacions_on_group_id"
-    t.index ["operation_id"], name: "index_group_operacions_on_operation_id"
-  end
 
   create_table "groups", force: :cascade do |t|
     t.bigint "author_id", null: false
@@ -30,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_183244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_groups_on_author_id"
+  end
+
+  create_table "groups_operations", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "operation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_groups_operations_on_group_id"
+    t.index ["operation_id"], name: "index_groups_operations_on_operation_id"
   end
 
   create_table "operations", force: :cascade do |t|
@@ -54,8 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_183244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "group_operacions", "groups"
-  add_foreign_key "group_operacions", "operations"
   add_foreign_key "groups", "users", column: "author_id"
+  add_foreign_key "groups_operations", "groups"
+  add_foreign_key "groups_operations", "operations"
   add_foreign_key "operations", "users", column: "author_id"
 end
