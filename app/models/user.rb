c116dfa0bcb7783c5008c92b4dc87bb35fdc62b1
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-09-08 19:26:58 +0200

    Add ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :groups, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
+
+  def admin?
+    role == 'admin'
+  end
 end
